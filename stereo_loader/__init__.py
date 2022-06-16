ef4428905baeebf04dbe45a92944cc157111dc98
jgieseler <jan.gieseler@utu.fi>
2022-06-16 18:31:45 +0300

    remove empty files before re-download

--- stereo_loader/__init__.py
+++ stereo_loader/__init__.py
@@ -390,7 +390,9 @@ def stereo_load(instrument, startdate, enddate, spacecraft='ahead', mag_coord='R
             downloaded_files = filelist
 
             for i, f in enumerate(filelist):
-                if not os.path.exists(f) or os.path.getsize(f) == 0:
+                if os.path.getsize(f) == 0:
+                    os.remove(f)
+                if not os.path.exists(f):
                     downloaded_file = Fido.fetch(result[0][i], path=path, max_conn=max_conn)
 
             # downloaded_files = Fido.fetch(result, path=path, max_conn=max_conn)
